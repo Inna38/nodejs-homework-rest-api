8e8b35e731e28cb9a650024a3e303ec0ebc58f80
Inna Didenko <innadidenko29@gmail.com>
2023-10-21 23:15:09 +0300

    create hw-02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,51 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const {nanoid} = require("nanoid")
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+const allContacts = await fs.readFile(contactsPath)
+ return JSON.parse(allContacts) 
+}
+
+const getContactById = async (contactId) => {
+  const contactById = await listContacts()
+  const result = contactById.find(({ id }) => id === contactId)
+  return result || null
+}
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+   ...body
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+
+
+
+//   const allContacts = await listContacts()
+//   const newContact = {
+//     id: nanoid(),
+//     ...body
+// }
+//   allContacts.push(newContact)
+//   await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2))
+//   return newContact
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const updateContact = async (contactId, body) => { }
 
-const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
   updateContact,
 }
