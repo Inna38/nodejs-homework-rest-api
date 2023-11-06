c9f314759cd58af34008f4ec13e05116f79e6b7b
Inna Didenko <innadidenko29@gmail.com>
2023-11-06 13:03:50 +0200

    create hw05-avatars

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const logger = require('morgan')
 const cors = require('cors')
 const dotenv = require("dotenv")
 
-
 const authRouter = require("./routes/api/auth")
 const contactsRouter = require('./routes/api/contacts')
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer')
+
+const path = require("path")
+
+const tempDir = path.join(__dirname, "temp")
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,  
+})
+
+const upload = multer({
+  storage: multerConfig
+})
 
 const {
   register,
@@ -11,10 +24,16 @@ const {
 
 const { authenticate } = require("../../middlewares");
 
+
 router.post("/users/register", register);
 router.post("/users/login", login);
 router.get("/users/current", authenticate, getCurrent);
 router.post("/users/logout", authenticate, logout);
-router.patch("/users/subscription", authenticate,  patchSubscription)
+router.patch("/users/subscription", authenticate, patchSubscription)
+router.patch("/users/avatars", authenticate, upload.single('avatar'), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+})
 
 module.exports = router;
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NDhiN2MyYjhiZmEwN2U2MWY2ZDE3YiIsImlhdCI6MTY5OTI2NDQ2MCwiZXhwIjoxNjk5MzQ3MjYwfQ.-rM6lQX8ROVE3md5IE5Ylk-KeudKtC8PQ-k6516YWJc
